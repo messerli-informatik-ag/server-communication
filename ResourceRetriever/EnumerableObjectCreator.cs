f2967dc803b9f4a7e314802aec6afcd8f57af19e
Dominic Groth <dominic.groth@messerli.ch>
2019-03-13 15:32:45 +0100

    Cleanup namespaces

--- ResourceRetriever/EnumerableObjectCreator.cs
+++ ResourceRetriever/EnumerableObjectCreator.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Utility.Utility.Extension;
+using Utility.Extension;
 
 namespace ResourceRetriever
 {
--- ResourceRetriever/JsonDeserializer.cs
+++ ResourceRetriever/JsonDeserializer.cs
@@ -1,6 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
-using Utility.Utility.Extension;
+using Utility.Extension;
 
 namespace ResourceRetriever
 {
