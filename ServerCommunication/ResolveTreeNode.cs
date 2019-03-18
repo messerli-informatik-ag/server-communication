4db8592b9fd9d0f47718743ace4c6e42235d8bdc
Dominic Groth <dominic.groth@messerli.ch>
2019-03-18 11:15:24 +0100

    Add ResolveTreeNode

---
+++ ServerCommunication/ResolveTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Messerli.ServerCommunication
+{
+    public class ResolveTreeNode
+    {
+        public ResolveTreeNode(Type currentType, object current, ResolveTreeNode parent)
+        {
+            CurrentType = currentType;
+            Current = current;
+            Parent = parent;
+        }
+
+        public Type CurrentType { get; }
+
+        public object Current { get; }
+
+        public ResolveTreeNode Parent { get; }
+    }
+}
